841ec30c796bb99e461fd15fb3e7c7cc008dd900
00004992 <00004992@AGI-LT-1046.arisglobal.com>
2021-09-23 15:36:40 +0530

    moven commit

---
+++ src/test/java/org/automationtest/TESTNG/Test1.java
@@ -0,0 +1,23 @@
+package org.automationtest.TESTNG;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+
+public class Test1 {
+	
+	
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.edge.driver","C:\\Users\\00004992\\Desktop\\Automation\\msedgedriver.exe");
+	
+		WebDriver driver=new EdgeDriver();
+		driver.get("https://google.com/");
+		String vTitle=driver.getTitle();
+		System.out.println("The title is" +vTitle);
+		Thread.sleep(5000);
+		driver.close();
+
+	}
+
+}
